d3f510f53d01ef704206d0823bea2fbcc79a72ad
Sharahachar <100009753+Sharahachar@users.noreply.github.com>
2024-07-03 15:30:22 +0530

    MainContent.js

--- MainContent.js
+++ MainContent.js
@@ -1,39 +1,31 @@
 // src/components/MainContent.js
-import React, { useState } from 'react';
+import React from 'react';
 import './MainContent.css';
 import img1 from "./S.jpg" ;
 import img2 from "./profile.png";
 
 const MainContent = () => {
-  const [message,setMessage] = useState("hello");
-
-   const handleClick = () => {
-    setMessage('search by employee name');
-  };
   return (
-  
     <main>
-       <div className="home-page">
+     <div className="home-page">
       <div className="logo-container">
         <img src={img1} alt=" Logo"className='logo' />
       </div>
-      <div className="profile-container">
-        <div className="profile">
-          <img src={img2} alt="profile " />
-          <h2 >Profile</h2>
-        </div>
-      </div>
-    </div>
-      <div className='lower'>
+            <div className="profile-container">
+               <div className="profile">
+                  <img src={img2} alt="profile " />
+                  <h2 >Profile</h2>
+               </div>
+           </div>
+     </div>
+     <div className='lower'>
         <div >
-        
-          <button onClick={handleClick} className="buttons">search by employee name</button>
+          <button className="buttons">search by employee name</button>
         </div>
         <div >
         <button className="buttons">search by Team/Project</button>
-        
-        </div>
-        </div>
+         </div>
+       </div>
     </main>
   );
 }
