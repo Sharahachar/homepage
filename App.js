a88904459fba29386514df08cb4b6c9fa35d35c9
Sharahachar <100009753+Sharahachar@users.noreply.github.com>
2024-07-03 13:58:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import MainContent from './MainContent';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    
+     <MainContent />
+    </div>
+  );
+}
+
+export default App;
